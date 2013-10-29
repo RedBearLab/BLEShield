f3b7822d1c844c189088079999f59aa16ca364e9
Cheong <yukcheong.poon@redbear.net>
2013-10-29 20:55:16 +0800

    Added BLE Shield library for Arduino. Support BLE Shield 1.x and 2.0

---
+++ Arduino/libraries/RBL_BLEShield/ble_shield.h
@@ -0,0 +1,28 @@
+/*
+
+Copyright (c) 2012, 2013 RedBearLab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+#ifndef  _BLE_SHIELD_H
+#define _BLE_SHIELD_H
+
+#include <services.h>
+
+void ble_begin();
+void ble_write(unsigned char data);
+void ble_write_bytes(unsigned char *data, unsigned char len);
+void ble_do_events();
+int ble_read();
+unsigned char ble_available();
+unsigned char ble_connected(void);
+void ble_set_pins(uint8_t reqn, uint8_t rdyn);
+
+#endif
+
